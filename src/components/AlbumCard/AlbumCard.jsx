f1db126d01d0de1162442c3034066fdc2a921155
oalwadi001 <omarwadi.2301@gmail.com>
2023-11-28 13:39:15 +0300

    Changed the way the ID is sent, changed files names, and did some changes to the album photos design

--- src/components/AlbumCard/AlbumCard.jsx
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -1,20 +1,14 @@
-import React, { useState, useEffect } from "react";
 import '../../components/Button/Button.css'
 import { Link} from 'react-router-dom';
-import "../Modal/Modal.css";
-import { Text,Card, CardHeader, CardBody, CardFooter} from '@chakra-ui/react'
-
-
+import { Text} from '@chakra-ui/react'
 export default function AlbumCard(props) {
-
 const { title, id } = props.album;
-
   return (
     <>
       <div className="album-card">
         <div className="content">
-          <Text fontSize='2xl' as='b' >{title}</Text>
-            <Link to={{ pathname: "/AlbumsPhotos", search: `?albumId=${id}` }} >
+          <Text fontSize='2xl' as='b'>{title}</Text>
+            <Link to={`/AlbumsPhotos/${id}`}>
               <button className="btn">
                 Show Photos
               </button>
--- src/pages/album-photos-page.jsx
+++ src/pages/album-photos-page.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useLocation} from 'react-router-dom';
+import { Link, useLocation, useParams} from 'react-router-dom';
+import { Spinner, Heading, Center, Divider } from '@chakra-ui/react'
 
 export default function AlbumPhotos(props) {
-    let location = useLocation();
-    let searchParams = new URLSearchParams(location.search);
-    let id = Number(searchParams.get("albumId"));
+    let {id} = useParams()
+    id = Number(id)
+    //let location = useLocation();
+    //let searchParams = new URLSearchParams(location.search);
+    //let id = Number(searchParams.get("albumId"));
     
     console.log(typeof(id))
     const [albumsImage, setalbumsImage] = useState("https://via.placeholder.com/600/92c952");
-
+    const [loader, setloader] = useState(false)
     const [photos, setPhotos] = useState([]);
 
     useEffect(() => {
@@ -22,6 +25,7 @@ export default function AlbumPhotos(props) {
     
           let photosData = await photosResponse.json();
           setPhotos(photosData);
+          setloader(true)
         } catch (error) {
           console.error("There has been a problem with your fetch operation:", error);
         }
@@ -33,17 +37,22 @@ export default function AlbumPhotos(props) {
     console.log("ID: ", id)
 
   return (
-    <div>
-        {
-        <div className="modal">
-          <div className="overlay"></div>
-          <div className="modal-content">
-            <img src={albumsImage} alt="Album Image" style={{marginBottom: "1rem"}}></img>
+    <>
+        {loader?
+          <div style={{margin: "3rem"}}>
+            <div>
+            <Center>
+              <Heading mb="2rem">Album Photos</Heading>
+            </Center>
+            </div>
+            <Center>
+              <img src={albumsImage} alt="Album Image" style={{marginBottom: "1rem", marginTop: "2rem"}}></img>
+            </Center>
             <div style={{display: "flex", flexDirection: "row", overflowX: "scroll",}}>
+
             {
               photos.map((photo, index) => {
                 if(id === photo.albumId){
-                  console.log("HEYYYYY")
                   return <img src={photo.thumbnailUrl} key={index} onClick={() => setalbumsImage(photo.url)}></img>
                 }
               })
@@ -57,8 +66,8 @@ export default function AlbumPhotos(props) {
                 </Link>
             </div>
           </div>
-        </div>
+        :<Spinner marginTop={"25%"} marginLeft={"50%"}/>
         }
-    </div>
+    </>
   )
 }
